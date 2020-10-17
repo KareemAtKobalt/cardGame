41ea8f31d5d29b7d3d548733d84e31f42a6d1d3f
kareem <kareem.hassan@ktech.com>
2020-10-17 14:56:09 +0100

    added ui changes to update cards when clicked (flipped or shown)

--- src/components/game/Card.js
+++ src/components/game/Card.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import classes from "./Card.css";
 import { clickOnCard } from "../../action/action";
+
+const cardStyle = [classes.Card];
 
 const renderSwitch = card => {
   if (card.number) {
@@ -27,7 +29,7 @@ const mapStateToProps = state => {
   return { firstSelection: state.playReducer.firstSelection };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     clickOnCard: (e, card) => {
       return dispatch(clickOnCard(card));
@@ -35,17 +37,36 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-const Card = props => {
-  const card = props.card;
+class Card extends Component {
+  state = {
+    cardHidden: false
+  };
 
-  return (
-    <div className={classes.Card} onClick={e => props.clickOnCard(e.target, card)}>
-      {renderSwitch(card)}
-      <br />
-      {card.suite}
-    </div>
-  );
-};
+  cardClassNames = [classes.Card];
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.card.id === this.props.card.id && !nextState.cardHidden;
+  }
+
+  onclickHandler = (event, card) => {
+    this.setState({ cardClicked: true });
+    this.cardClassNames.push(classes.CardFlipped);
+    this.props.clickOnCard(event, card);
+  };
+
+  render() {
+    return (
+      <div
+        className={this.cardClassNames.join(" ")}
+        onClick={e => this.onclickHandler(e.target, this.props.card)}
+      >
+        {renderSwitch(this.props.card)}
+        <br />
+        {this.props.card.suite}
+      </div>
+    );
+  }
+}
 
 export default connect(
   mapStateToProps,
